479169fe77ec4716654885d9e0c48e0f1501ce19
ICEIN <luisfajardo@indugravas.com>
2026-01-22 08:33:34 -0500

    Refactor get_drive_service to use TOML format for secrets
    Updated the get_drive_service function to use secrets in TOML format instead of JSON.

--- drive_utils.py
+++ drive_utils.py
@@ -1,6 +1,4 @@
 # drive_utils.py
-import io
-import json
 import streamlit as st
 from google.oauth2 import service_account
 from googleapiclient.discovery import build
@@ -8,11 +6,16 @@ from googleapiclient.http import MediaIoBaseDownload
 
 SCOPES = ["https://www.googleapis.com/auth/drive"]
 
+
 def get_drive_service():
-    sa_json = st.secrets["GDRIVE_SERVICE_ACCOUNT_JSON"]
-    info = json.loads(sa_json)
+    """
+    Crea el cliente de Google Drive usando secrets en formato TOML:
+    [google_service_account] ...
+    """
+    info = dict(st.secrets["google_service_account"])
     creds = service_account.Credentials.from_service_account_info(info, scopes=SCOPES)
     return build("drive", "v3", credentials=creds)
+
 
 def list_folders(service, parent_id: str):
     q = (
@@ -22,6 +25,7 @@ def list_folders(service, parent_id: str):
     )
     res = service.files().list(q=q, fields="files(id,name)", pageSize=100).execute()
     return res.get("files", [])
+
 
 def find_child_folder(service, parent_id: str, name: str):
     q = (
@@ -34,6 +38,7 @@ def find_child_folder(service, parent_id: str, name: str):
     files = res.get("files", [])
     return files[0]["id"] if files else None
 
+
 def find_file(service, parent_id: str, filename: str):
     q = (
         f"'{parent_id}' in parents "
@@ -44,6 +49,7 @@ def find_file(service, parent_id: str, filename: str):
     files = res.get("files", [])
     return files[0]["id"] if files else None
 
+
 def download_file(service, file_id: str, dest_path):
     request = service.files().get_media(fileId=file_id)
     with open(dest_path, "wb") as fh:
@@ -51,3 +57,4 @@ def download_file(service, file_id: str, dest_path):
         done = False
         while not done:
             _, done = downloader.next_chunk()
+
